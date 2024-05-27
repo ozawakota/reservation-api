0093c407ba18991ec1959baa6fe20de4d8cd3169
k_ozawa <kouta.ozawa@nadia.bz>
2024-05-27 18:44:02 +0900

    予約システム

--- app.py
+++ app.py
@@ -64,19 +64,19 @@ elif page == 'bookings':
   res = requests.get(url_users)
   users = res.json()
   # ユーザー名をキーにして、ユーザーIDをバリュー
-  users_dict = {}
+  users_name = {}
   for user in users:
-    users_dict[user['username']] = user['user_id']
+    users_name[user['username']] = user['user_id']
 
-  # st.write(users_dict)
+  # st.write(users_name)
 
   # 会議室一覧の取得
   url_rooms = URL_LOCALHOST + '/rooms'
   res = requests.get(url_rooms)
   rooms = res.json()
-  rooms_dict = {}
+  rooms_name = {}
   for room in rooms:
-    rooms_dict[room['room_name']] = {
+    rooms_name[room['room_name']] = {
       'room_id' : room['room_id'],
       'capacity' : room['capacity']
     }
@@ -86,9 +86,49 @@ elif page == 'bookings':
   df_rooms.columns = ['会議室', '定員', '会議室ID']
   st.table(df_rooms)
 
+  url_bookings = URL_LOCALHOST + '/bookings'
+  res = requests.get(url_bookings)
+  bookings = res.json()
+  df_bookings = pd.DataFrame(bookings)
+
+  users_id = {}
+  for user in users:
+    users_id[user['user_id']] = user['username']
+
+  rooms_id = {}
+  for room in rooms:
+    rooms_id[room['room_id']] = {
+      'room_name': room['room_name'],
+      'capacity': room['capacity']
+    }
+
+  # IDを各値に変更(ラムダ関数)
+  to_username = lambda x: users_id[x]
+  to_room_name = lambda x: rooms_id[x]['room_name']
+  to_datetime = lambda x: datetime.datetime.fromisoformat(x).strftime('%Y/%m/%d %H:%M')
+
+  # 特定の列に適用
+  df_bookings['user_id'] = df_bookings['user_id'].map(to_username)
+  df_bookings['room_id'] = df_bookings['room_id'].map(to_room_name)
+  df_bookings['start_datetime'] = df_bookings['start_datetime'].map(to_datetime)
+  df_bookings['end_datetime'] = df_bookings['end_datetime'].map(to_datetime)
+
+  df_bookings = df_bookings.rename(columns={
+      'user_id': '予約者名',
+      'room_id': '会議室名',
+      'booked_num': '予約人数',
+      'start_datetime': '開始時刻',
+      'end_datetime': '終了時刻',
+      'booking_id': '予約番号'
+    })
+
+  st.write('### 予約一覧')
+  st.table(df_bookings)
+
+
   with st.form(key='booking'):
-    username: str = st.selectbox('予約者名', users_dict.keys())
-    room_name: str = st.selectbox('会議室名', rooms_dict.keys())
+    username: str = st.selectbox('予約者名', users_name.keys())
+    room_name: str = st.selectbox('会議室名', rooms_name.keys())
     booked_num: int = st.number_input('予約人数', step=1, min_value=1)
     date = st.date_input('日付: ', min_value=datetime.date.today())
     start_time = st.time_input('開始時刻: ', value=datetime.time(hour=9, minute=0))
@@ -96,9 +136,9 @@ elif page == 'bookings':
     submit_button = st.form_submit_button(label='予約登録')
 
   if submit_button:
-    user_id: int = users_dict[username]
-    room_id: int = rooms_dict[room_name]['room_id']
-    capacity: int = rooms_dict[room_name]['capacity']
+    user_id: int = users_name[username]
+    room_id: int = rooms_name[room_name]['room_id']
+    capacity: int = rooms_name[room_name]['capacity']
 
     data = {
       'user_id': user_id,
@@ -119,8 +159,16 @@ elif page == 'bookings':
         minute=end_time.minute
       ).isoformat(),
     }
-    # 定員以下の予約人数の場合
-    if booked_num <= capacity:
+    # 定員より多い予約人数の場合
+    if booked_num > capacity:
+      st.error(f'{room_name}の定員は、{capacity}名です。{capacity}名以下の予約人数のみ受け付けております。')
+    # 開始時刻 >= 終了時刻
+    elif start_time >= end_time:
+      st.error('開始時刻が終了時刻を超えています')
+    elif start_time < datetime.time(hour=9, minute=0, second=0) or end_time > datetime.time(hour=20, minute=0, second=0):
+      st.error('利用時間は9:00~20:00になります。')
+
+    else:
       # 会議室の予約を行う
       url = URL_LOCALHOST + '/bookings'
       res = requests.post(
@@ -129,7 +177,6 @@ elif page == 'bookings':
       )
       if res.status_code == 200:
         st.success('予約完了しました')
-      st.json(res.json())
+      elif res.status_code == 404 and res.json()['detail'] == 'Already booked':
+        st.error('指定の時間にはすでに予約が入っています。')
 
-    else:
-      st.error(f'{room_name}の定員は、{capacity}名です。{capacity}名以下の予約人数のみ受け付けております。')
--- sql_app/main.py
+++ sql_app/main.py
@@ -24,17 +24,17 @@ def get_db():
 
 # Read
 @app.get("/users", response_model=List[schemas.User])
-async def read_users(skip: int =0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
   users = crud.get_users(db, skip=skip, limit=limit)
   return users
 
 @app.get("/rooms", response_model=List[schemas.Room])
-async def read_rooms(skip: int =0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_rooms(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
   rooms = crud.get_rooms(db, skip=skip, limit=limit)
   return rooms
 
 @app.get("/bookings", response_model=List[schemas.Booking])
-async def read_bookings(skip: int =0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_bookings(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
   bookings = crud.get_bookings(db, skip=skip, limit=limit)
   return bookings
 
